74918c7dc983e4d556ebc74b599229dc74f29b75
Ubuntu <ubuntu@ip-172-31-18-39.ec2.internal>
2026-04-19 11:49:26 +0000

    Fixed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f"<h1>Hello from Kubernetes!</h1><p>Running on: {os.uname()[1]}</p>"
+    return f"<h1>Hello from Kubernetes, welcome!</h1><p>Running on: {os.uname()[1]}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
