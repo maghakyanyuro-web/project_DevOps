b0facfa11ca1eba91b6a9737d391d6479e6edfa7
Ubuntu <ubuntu@ip-172-31-18-39.ec2.internal>
2026-04-19 14:37:19 +0000

    Fixing

--- main.tf
+++ main.tf
@@ -1,9 +1,44 @@
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Allow SSH and HTTP inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "devops_nodes" {
   count		= 2
   ami		= "ami-0ec10929233384c7f"
-  instance_type = "t3.micro"
+  instance_type = "m7i-flex.large"
 
   iam_instance_profile = "Terraform-Manager-Role"
+  
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
   tags = {
     Name = "DevOps-Master-Node"
