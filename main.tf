a3b6f7acfeab8eb82c975d5a77a41176b3fcf992
Ubuntu <ubuntu@ip-172-31-18-39.ec2.internal>
2026-04-22 15:14:24 +0000

    test

--- main.tf
+++ main.tf
@@ -9,7 +9,14 @@ resource "aws_security_group" "allow_ssh_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+ingress {
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+ 
+ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
